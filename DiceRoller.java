af3de9ab97eedabfe3e2f50c828b4fd556b4c7e6
Mark Marvin <133226807+MarvinKinyanjui@users.noreply.github.com>
2025-03-25 19:37:58 +0300

    Update DiceRoller.java

--- DiceRoller.java
+++ DiceRoller.java
@@ -1,3 +1,13 @@
-public class DiceRoller {
+import java.util.Random;
 
+public class DiceRoller {
+public void rollDice() {
+        Random dice = new Random();
+        int roll1 = dice.nextInt();
+        int roll2 = dice.nextInt(101);
+        int roll3 = dice.nextInt(50, 101);
+        System.out.println("First roll: " + roll1);
+        System.out.println("Second roll: " + roll2);
+        System.out.println("Third roll: " + roll3);
+    }
 }
