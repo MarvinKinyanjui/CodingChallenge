72f4c972b0d842eaed88cbf74c775dcfda86b439
Mark Marvin <133226807+MarvinKinyanjui@users.noreply.github.com>
2025-03-25 19:38:50 +0300

    Update IncrementDecrementOrderDemo.java

--- IncrementDecrementOrderDemo.java
+++ IncrementDecrementOrderDemo.java
@@ -1,3 +1,15 @@
 public class IncrementDecrementOrderDemo {
+    public void demoIncrementDecrementOrder() {
+        int value = 1;
+        value++;
+        System.out.println("First: " + value);
+        System.out.println("Second: " + value++);
+        System.out.println("Third: " + value);
+        System.out.println("First: " + (++value));
 
+        value = 1;
+        ++value;
+        value++;
+        System.out.println("First: " + value);
+    }
 }
