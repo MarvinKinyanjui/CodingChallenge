8b3c732886ed00e1314fcbe31733b6028bce29b0
Mark Marvin <133226807+MarvinKinyanjui@users.noreply.github.com>
2025-03-25 19:40:13 +0300

    Update TemperatureConverter.java

--- TemperatureConverter.java
+++ TemperatureConverter.java
@@ -1,3 +1,11 @@
 public class TemperatureConverter {
+    public void convertToCelsius(int fahrenheit) {
+        double temperature = ((fahrenheit - 32) * 5.0 / 9);
+        System.out.println("The temperature is " + temperature + " degrees Celsius.");
+    }
 
+    public void convertToFahrenheit(double celsius) {
+        double temperature = (celsius * 9.0 / 5) + 32;
+        System.out.println("The temperature is " + temperature + " degrees Fahrenheit.");
+    }
 }
