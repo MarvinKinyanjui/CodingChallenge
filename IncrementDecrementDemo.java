714fc2d7875182d16e9993561e6d1cfa43da983f
Mark Marvin <133226807+MarvinKinyanjui@users.noreply.github.com>
2025-03-25 19:38:21 +0300

    Update IncrementDecrementDemo.java

--- IncrementDecrementDemo.java
+++ IncrementDecrementDemo.java
@@ -1,3 +1,18 @@
-public class IncrementDecementDemo {
-
+public class IncrementDecrementDemo {
+    public void demoIncrementDecrement() {
+        int value = 1;
+        value = value + 1;
+        System.out.println("First Increment: " + value);
+        value += 1;
+        System.out.println("Second Increment: " + value);
+        value++;
+        System.out.println("Third Increment: " + value);
+        value = value - 1;
+        System.out.println("First Decrement: " + value);
+        value -= 1;
+        System.out.println("Second Decrement: " + value);
+        value--;
+        System.out.println("Third Decrement: " + value);
+    }
 }
+
